845e6966889519dd2024f84a018911aa48e64630
ploydelays <ployjae@gmail.com>
2017-10-03 12:53:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,17 @@
-<?php
-echo "I am a bot(bot.php)";
+
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer p2tNOuR1m5zaEpjpvWf61x5TBrKEsjhqrXmBwmVpG8XqA2ui4/Ww2HTQaQEJO4rQDwzZnQ2dCycskHDFRuzs3AfEDvnB7m3a3VMuLIkrYcq3WCcixq7ElnrdrAHcJ4c3N+n5eUzSCoNUWZjrNeyj9QdB04t89/1O/w1cDnyilFU=' \
+-d '{
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/reply
